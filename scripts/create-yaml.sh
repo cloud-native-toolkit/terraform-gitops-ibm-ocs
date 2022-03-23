10489614c1e26ec007844c5ccf8aca7195b47c68
Andrew Trice <amtrice@us.ibm.com>
2022-03-23 15:04:30 -0400

    rename to odf (#2)
    * rename to odf
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    
    * updated-actions
    
    * update sealed secrets version
    
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>
    Signed-off-by: Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
-CHART_DIR=$(cd "${MODULE_DIR}/chart/ibm-ocs"; pwd -P)
+CHART_DIR=$(cd "${MODULE_DIR}/chart/ibm-odf"; pwd -P)
 
 NAME="$1"
 DEST_DIR="$2"
