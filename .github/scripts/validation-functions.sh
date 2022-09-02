05ab38cd24571b817e799efe31527a76634e43b3
Sean Sundberg <seansund@us.ibm.com>
2022-09-02 06:56:46 -0500

    Updates check_k8s_resource to handle wildcard name
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validation-functions.sh
+++ .github/scripts/validation-functions.sh
@@ -52,7 +52,7 @@ check_k8s_resource () {
   local NAME="$3"
 
   count=0
-  until kubectl get "${GITOPS_TYPE}" "${NAME}" -n "${NS}" 1> /dev/null 2> /dev/null || [[ $count -gt 20 ]]; do
+  until [[ $(kubectl get "${GITOPS_TYPE}" -n "${NS}" -o json | jq -r --arg NAME "${NAME}" '.items[] | .metadata.name | select(. | test($NAME))' | wc -l) -gt 0 ]] || [[ $count -gt 20 ]]; do
     echo "Waiting for ${GITOPS_TYPE}/${NAME} in ${NS}"
     count=$((count + 1))
     sleep 30
@@ -64,10 +64,12 @@ check_k8s_resource () {
     exit 1
   fi
 
-  kubectl get "${GITOPS_TYPE}" "${NAME}" -n "${NS}" -o yaml
+  local full_name=$(kubectl get "${GITOPS_TYPE}" -n "${NS}" -o json | jq -r --arg NAME "${NAME}" '.items[] | .metadata.name | select(. | test($NAME))' | head -1)
+
+  kubectl get "${GITOPS_TYPE}" "${full_name}" -n "${NS}" -o yaml
 
   if [[ "${GITOPS_TYPE}" =~ deployment|statefulset|daemonset ]]; then
-    kubectl rollout status "${GITOPS_TYPE}" "${NAME}" -n "${NS}"
+    kubectl rollout status "${GITOPS_TYPE}" "${full_name}" -n "${NS}"
   elif [[ "${GITOPS_TYPE}" == "job" ]]; then
     kubectl wait --for=condition=complete "job/${NAME}" -n "${NS}"
   fi
