8f29c1c19eab16f4a8082a04322e7a4bed0f42dc
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2022-09-30 16:50:48 -0500

    added variables to mymodule
    Signed-off-by: Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -7,4 +7,17 @@ module "gitops_module" {
   namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
   ibmcloud_api_key = var.ibmcloud_api_key
+  osdStorageClassName = var.osdStorageClassName
+  osdSize = var.osdSize
+  numOfOsd = var.numOfOsd
+  billingType = var.billingType
+  ocsUpgrade = var.ocsUpgrade
+  clusterEncryption = var.clusterEncryption
+  monSize = var.monSize
+  monStorageClassName = var.monStorageClassName
+  hpcsServiceName = var.hpcsServiceName
+  hpcsInstanceId = var.hpcsInstanceId
+  hpcsSecretName = var.hpcsSecretName
+  hpcsBaseUrl = var.hpcsBaseUrl
+  hpcsTokenUrl = var.hpcsTokenUrl
 }
