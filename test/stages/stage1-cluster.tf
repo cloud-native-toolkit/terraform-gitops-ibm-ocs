1c524cce498ab377054ebc6f5b4771aa25a88456
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2022-10-04 11:53:46 -0500

    removed old code
    Signed-off-by: Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -1,23 +1,3 @@
-# module "cluster" {
-#   source = "github.com/cloud-native-toolkit/terraform-ibm-ocp-vpc.git"
-
-#   depends_on = [ module.cos, module.subnets, module.vpc ]
-
-#   resource_group_name = module.resource_group.name
-#   region              = var.region
-#   ibmcloud_api_key    = var.ibmcloud_api_key
-#   name                = var.cluster_name
-#   worker_count        = var.worker_count
-#   ocp_version         = var.ocp_version
-#   exists              = "false"
-#   name_prefix         = local.name_prefix_test
-#   vpc_name            = module.subnets.vpc_name
-#   vpc_subnets         = module.subnets.subnets
-#   vpc_subnet_count    = module.subnets.count
-#   cos_id              = module.cos.id
-#   login               = "true"
-# }
-
 module "cluster" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-ocp-vpc.git"
 
